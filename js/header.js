1281c88854799ec1f28c47544ce783278957b7e8
Omphile Mohlala <omphilestudent@gmail.com>
2025-08-26 15:01:28 +0200

    Starting with postilion portal

---
+++ js/header.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header");
+
+    const headerContent = `
+    <div class="header-content">
+                <div class="header-item">
+                    <div class="items">
+                        <span>User:</span>
+                        <span>CP378455</span>
+                    </div>
+                    <div class="items" style="position: absolute; left: -1px; top: 50px;">
+                        <span>Last Login:</span>
+                        <span>None</span>
+                    </div>
+                    <div class="items">
+                        <span>Financial Institution:</span>
+                        <span>Business</span>
+                    </div>
+                    <div class="items" style="position: absolute; left: 260px; top: 50px;">
+                        <span>Institution ID:</span>
+                        <span></span>
+                    </div>
+                    <div class="items">
+                        <span>Issuer:</span>
+                        <span>CapitecBusiness</span>
+                    </div>
+                    <div class="items" style="position: absolute; top: 50px; left: 610px;">
+                        <span>Participant:</span>
+                        <span>Capitec Bank</span>
+                    </div>
+                </div>
+                <div class="bottom-items-right">
+                    <span>My Profile|</span>
+                    <span>Logout|</span>
+                    <span>HowTo|</span>
+                    <span>About</span>
+                </div>
+            </div>`;
+
+    if (header) {
+        header.innerHTML = headerContent;
+    }
+
+    }
+);
---
+++ js/side-bar.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sideBar = document.querySelector(".side-bar");
+
+    const sideBarContent = `
+    <div class="head" style="width: 50px;"><img src="../img/head.png" alt="head" class="img"></div>
+    <div class="side-bar-content">
+    <div class="side-bar-item">Account Products</div>
+    <div class="side-bar-item">Accounts</div>
+    <div class="side-bar-item">Cards</div>
+    <div class="side-bar-item">Card programs</div>
+    <div class="side-bar-item">Employees</div>
+    <div class="side-bar-item">Hold Response code</div>
+    <div class="side-bar-item">Retail Customers</div>
+    <div class="side-bar-item">Transactions</div>
+    <div class="side-bar-item">Historic Tran Search</div>
+    
+</div>
+  `;
+
+    if (sideBar) {
+        sideBar.innerHTML = sideBarContent;
+    }
+});
